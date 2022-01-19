0fb0ff8796c337ab65978c7307656830b741bee8
Roman Gladyshev <romagladyshev@gmail.com>
2022-01-19 19:20:51 +0600

    setup heroku

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,12 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const config: TypeOrmModuleOptions = {
-  type: 'mysql',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  host: 'localhost',
-  database: 'puregram_db',
+  url: process.env.DATABASE_URL,
+  type: 'postgres',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // port: 3306,
+  // username: 'root',
+  // password: 'root',
+  // host: 'localhost',
+  // database: 'puregram_db',
   synchronize: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
 };
