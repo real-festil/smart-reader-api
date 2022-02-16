6a0d0b095f230f40f13de86d9ce22f65d3c766d7
Roman Gladyshev <romangladyshev@MacBook-Pro-Roman.local>
2022-02-16 20:34:39 +0600

    change orm config

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -6,7 +6,7 @@ export const config: TypeOrmModuleOptions = {
   // ssl: {
   //   rejectUnauthorized: false,
   // },
-  port: 1434,
+  port: 3306,
   username: 'root',
   password: 'root',
   host: 'localhost',
